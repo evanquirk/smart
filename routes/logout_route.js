a69db80f8736b44204bc895e9c7bfcc99cf45cdb
Evan Quirk <evanquirk>
2021-03-07 18:15:42 -0800

    feat: add login and logout route handling

---
+++ routes/logout_route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = () => {
+  //logs user out and clears the cookie session
+  router.post("/", (req, res) => {
+   req.session["user"] = null;
+   res.redirect("/")
+  });
+
+  return router;
+};
