faab5328075f455e0985a7055d14bebf791dde65
Evan Quirk <evanquirk>
2021-03-07 21:38:20 -0800

    feat: add login logout routes

--- server.js
+++ server.js
@@ -34,12 +34,14 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const loginRoute = require("./routes/login_route");
+const logoutRoute = require("./routes/logout_route");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/users", usersRoutes(db));
+app.use("/login", loginRoute(db));
+app.use("/logout", logoutRoute(db));
 // Note: mount other resources here, using the same pattern above
 
 
