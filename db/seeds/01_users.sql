349499c20394f632e583114ac9d872bb9ddfc894
Evan Quirk <evanquirk>
2021-03-08 19:30:31 -0800

    chore: add test users to seeds file

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, email, password) VALUES ('Evan', 'e@e.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('Thomas', 't@t.com', 'password');
