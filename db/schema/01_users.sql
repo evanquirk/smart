c8d5b9d3facb3203eb85776a380838abe0308d77
Evan Quirk <evanquirk>
2021-03-08 19:57:39 -0800

    fix: split schema into seperate files

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,8 +1,7 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS lists CASCADE;
-DROP TABLE IF EXISTS to_dos CASCADE;
+
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -11,18 +10,3 @@ CREATE TABLE users (
   email VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE lists (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  is_public BOOLEAN NOT NULL DEFAULT FALSE
-);
-
-CREATE TABLE to_dos (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  lists_id INTEGER REFERENCES lists(id) ON DELETE CASCADE,
-  to_do_name VARCHAR(255) NOT NULL,
-  api_source NOT NULL,
-  to_do_description VARCHAR(255) NOT NULL,
-  date_created DATE NOT NULL
-);
