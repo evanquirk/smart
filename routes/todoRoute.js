33724c45fff184b4b17c1e73ad85fe2e107af9ca
Evan Quirk <evanquirk>
2021-03-11 14:43:40 -0800

    refactor: change the toDo route as app is moved to single page

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router  = express.Router();
-const { insertSearchResults, getSearchResults } = require('../helpers/toDoQueries');
+const { insertSearchResults, getSearchResults, deleteSearchResults, deleteTodo } = require('../helpers/toDoQueries');
 const { getUserById } = require('../helpers/authQueries');
+
 const { searchYelp } = require('../api-helpers/yelp');
 const { searchBooks } = require('../api-helpers/books');
 const { searchMovies } = require('../api-helpers/movies');
@@ -11,6 +12,12 @@ const { searchItems } = require('../api-helpers/grocery-items');
 module.exports = () => {
 
   router.post("/", async (req, res) => {
+    const searchResults = await getSearchResults();
+    console.log(searchResults);
+    if (searchResults) {
+      await deleteSearchResults();
+    }
+
     const todoSearch = req.body.todo
     const yelpPromise = searchYelp(todoSearch)
     const bookPromise = searchBooks(todoSearch)
@@ -22,52 +29,18 @@ module.exports = () => {
       return [...values[0],...values[1],...values[2],...grocery]
     }).then(results => {
       return insertSearchResults(results)
-    }).finally(() => res.redirect('/todo'))
+    }).finally(() => res.redirect('/user-lists'))
   });
 
 
   //===========================================
 
-  router.get("/", async (req, res) => {
-    const user = await getUserById(req.session.user_id);
-    const searchResults = await getSearchResults();
-    let results
-    // const toWatch =[];
-    // const toRead =[];
-    // const toEat =[];
-    // const toBuy =[];
-    // const uncategorized =[];
+  router.post("/:id/delete", async (req, res) => {
+    const toDoId = req.params.id
 
-    if (!searchResults) {
-      results = null
-    } else {
-      // console.log('RESULTS:',JSON.parse(searchResults.results));
-      results = JSON.parse(searchResults.results)
-      // for (result of results) {
-      //   if (result.type === 'to_watch') {
-      //     toWatch.push(result)
-      //   } else if (result.type === 'to_read') {
-      //     toRead.push(result)
-      //   } else if (result.type === 'to_eat') {
-      //     toEat.push(result)
-      //   } else if (result.type === 'to_buy') {
-      //     toBuy.push(result)
-      //   } else {
-      //     uncategorized.push(result)
-      //   }
-      // }
-    }
-    const templateVars = {
-      user,
-      results,
-      // toWatch,
-      // toRead,
-      // toEat,
-      // toBuy,
-      // uncategorized
-    }
+    await deleteTodo(toDoId)
 
-   res.render("../views/todo.ejs", templateVars)
+   res.redirect("/user-lists")
 
   });
 
