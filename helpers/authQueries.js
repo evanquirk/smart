95ed4a4fe184b47be25ec9ff4b01b1d85925a2ba
Evan Quirk <evanquirk>
2021-03-11 10:08:17 -0800

    fix: clean up code and move functions to appropriate queries file

--- helpers/authQueries.js
+++ helpers/authQueries.js
@@ -1,6 +1,8 @@
 const { db } = require('../db/index');
 const bcrypt = require('bcrypt');
 
+//================================================
+
 const getUserByEmail = async (email) => {
   const queryString = `
   SELECT * FROM users
@@ -15,46 +17,49 @@ const getUserByEmail = async (email) => {
     console.error('query error', err.stack);
   }
 }
-  // add user to the database using bcrypt.
-  const addUser = async (obj) => {
-    const { name, email, password } = obj;
 
-    const queryString = `
+//================================================
+
+const addUser = async (obj) => {
+  const { name, email, password } = obj;
+
+  const queryString = `
     INSERT INTO users (username, email, password)
     VALUES ($1, $2, $3)
     RETURNING *;
   `;
 
-    const queryParams = [name, email, bcrypt.hashSync(password, 10)];
+  const queryParams = [name, email, bcrypt.hashSync(password, 10)];
 
-    try {
-      const res = await db.query(queryString, queryParams);
-      return res.rows[0];
+  try {
+    const res = await db.query(queryString, queryParams);
+    return res.rows[0];
 
-    } catch (err) {
-      console.error('query error', err.stack);
+  } catch (err) {
+    console.error('query error', err.stack);
 
-    }
   }
+}
 
-  const getUserById = async (id) => {
-    const queryString = `
+//===================================================
+
+const getUserById = async (id) => {
+  const queryString = `
       SELECT *
       FROM users
       WHERE id = $1
     `;
-    const queryParams = [id];
+  const queryParams = [id];
 
-    try {
-      const res = await db.query(queryString, queryParams);
-      // console.log('RESROWS:', res.rows[0]);
-      return res.rows[0];
+  try {
+    const res = await db.query(queryString, queryParams);
+    return res.rows[0];
 
-    } catch (err) {
-      console.error('query error', err.stack);
-    }
+  } catch (err) {
+    console.error('query error', err.stack);
   }
+}
 
 
 
-module.exports = { getUserByEmail , addUser , getUserById };
+module.exports = { getUserByEmail, addUser, getUserById };
