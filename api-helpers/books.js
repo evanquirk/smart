3dc9fa3d2078dfc4aeaef28f74dc9316f9105a34
Thomas Weideman <thomas.weideman@gmail.com>
2021-03-11 18:05:02 +0000

    Fix searchBooks to not return undefined values

--- api-helpers/books.js
+++ api-helpers/books.js
@@ -14,9 +14,21 @@ const searchBooks = function (searchTerms) {
         if (response.items[0].volumeInfo.authors) {
           result1.description = response.items[0].volumeInfo.authors[0];
         }
-        if (response.items[0].volumeInfo.publishedDate) {
+        if (response.items[0].volumeInfo.authors === undefined) {
+          result1.description = "";
+        }
+        if (
+          response.items[0].volumeInfo.publishedDate &&
+          response.items[0].volumeInfo.authors !== undefined
+        ) {
           result1.description +=
             ", " + response.items[0].volumeInfo.publishedDate.slice(0, 4);
+        }
+        if (
+          response.items[0].volumeInfo.publishedDate &&
+          response.items[0].volumeInfo.authors === undefined
+        ) {
+          result1.description = response.items[0].volumeInfo.publishedDate.slice(0, 4);
         }
         if (response.items[0].volumeInfo.description) {
           result1.description +=
@@ -28,9 +40,21 @@ const searchBooks = function (searchTerms) {
         if (response.items[1].volumeInfo.authors) {
           result2.description = response.items[1].volumeInfo.authors[0];
         }
-        if (response.items[1].volumeInfo.publishedDate) {
+        if (response.items[1].volumeInfo.authors === undefined) {
+          result2.description = "";
+        }
+        if (
+          response.items[1].volumeInfo.publishedDate &&
+          response.items[1].volumeInfo.authors !== undefined
+        ) {
           result2.description +=
             ", " + response.items[1].volumeInfo.publishedDate.slice(0, 4);
+        }
+        if (
+          response.items[1].volumeInfo.publishedDate &&
+          response.items[1].volumeInfo.authors === undefined
+        ) {
+          result2.description = response.items[1].volumeInfo.publishedDate.slice(0, 4);
         }
         if (response.items[1].volumeInfo.description) {
           result2.description +=
@@ -39,13 +63,9 @@ const searchBooks = function (searchTerms) {
         result2.type = "to_read";
         results.push(result2);
       }
-<<<<<<< HEAD
       console.log(results);
-=======
->>>>>>> 9ec69f0bccf67d70eced497582a073fbf3cce793
       return results;
     });
 };
-
 
 module.exports = { searchBooks };
