ebdb9e7c32e5da16aba1fa0a4a9786b493589038
Evan Quirk <71678276+evanquirk@users.noreply.github.com>
2021-03-11 14:44:58 -0800

    Feat/search api (#27)
    * refactor: move preview to single page
    
    * feat: add deleteToDo, deleteSearchResults functions
    
    * refactor: change the toDo route as app is moved to single page
    
    Co-authored-by: Evan Quirk <evanquirk>

--- routes/userListsRoute.js
+++ routes/userListsRoute.js
@@ -2,21 +2,25 @@ const express = require('express');
 const router  = express.Router();
 const { getUserById } = require('../helpers/authQueries');
 const { listById } = require('../helpers/listsQueries');
-const { toDoById , addNewToDo } = require('../helpers/toDoQueries');
+const { toDoById , addNewToDo, deleteSearchResults, getSearchResults } = require('../helpers/toDoQueries');
 
 
 module.exports = () => {
   router.get("/", async (req, res) => {
     const userId = req.session.user_id;
-
     const user = await getUserById(userId);
     const lists = await listById(userId);
     const toDos = await toDoById(userId);
-    const templateVars = { user, lists, toDos };
+    const searchResults = await getSearchResults();
 
-    //query database for user lists (returns an array of lists)
-    //query database for users to dos (this will be in a loop because of the users lists) (returns either obj/arr of todos)
-    //pass all info to templateVars which will populate on page
+    let results;
+    if (!searchResults) {
+      results = null
+    } else {
+      results = JSON.parse(searchResults.results)
+    }
+
+    const templateVars = { user, lists, toDos, results };
 
    res.render("../views/user_lists.ejs", templateVars)
   });
@@ -29,16 +33,11 @@ module.exports = () => {
     const name = req.body.name;
     const description = req.body.description;
 
-    // \/\/  NEED API KEY FOR FUNCTION TO WORK CORRECTLY. HANDLE IN MORNING! \/\/
-
     const input = { userId, type, name, description }
     //  Add these values to Lists Database
-    const newToDo = await addNewToDo(input);
-
-    // Empty search list
-    // throw in some jQuery to alert the user which path they want to go - redirect to /user-lists or /to-do
+    await addNewToDo(input);
+    await deleteSearchResults();
     res.redirect('/user-lists')
-
   });
 
   return router;
