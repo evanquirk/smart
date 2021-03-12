a6d157b008681b912cf78fc3c60a8b8300a0a0de
Evan Quirk <evanquirk>
2021-03-11 16:25:23 -0800

    fix logout clears search params, change To Eat to To Visit Nearby

--- routes/logoutRoute.js
+++ routes/logoutRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const { deleteSearchResults } = require('../helpers/toDoQueries')
 
 module.exports = () => {
   //logs user out and clears the cookie session
-  router.post("/", (req, res) => {
+  router.post("/", async (req, res) => {
    req.session = null;
+   await deleteSearchResults();
    res.redirect("/")
   });
 
